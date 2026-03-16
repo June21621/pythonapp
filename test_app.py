35cbccf6f152d9bb03a5c09cc26ff8048799162a
bootcamp computer <kymoon4@naver.com>
2026-03-16 10:48:46 +0900

    pytonapp

---
+++ test_app.py
@@ -0,0 +1,11 @@
+import pytest
+
+from utils import get_movie_info
+
+def test_get_movie_info():
+    test_url = "https://movie.naver.com/movie/bi/mi/basic.nhn?code=185293"
+    title, image, desc = get_movie_info(test_url)
+    print(title)
+    print(image)
+    print(desc)
+    assert title == "내일의 기억 : 네이버 검색"
